428a9dead82f8b3db8b090f8a067d3482fae11c3
Beatriz <101806629+Chagasbiaa@users.noreply.github.com>
2023-09-05 19:55:43 -0300

    tela pix

--- app/src/main/java/com/example/pagamento/MainActivity.java
+++ app/src/main/java/com/example/pagamento/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.pagamento;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button pix;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pix = findViewById(R.id.button2);
+
+        pix.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, telapix.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/pagamento/copiadopix.java
@@ -0,0 +1,14 @@
+package com.example.pagamento;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class copiadopix extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_copiadopix);
+    }
+}
